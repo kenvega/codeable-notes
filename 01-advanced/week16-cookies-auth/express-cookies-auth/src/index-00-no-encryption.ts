ac0c4fd70bfe035fdfc3c5a5591fa3dc82cb5b6c
Ken Vega <ken.vega.g@gmail.com>
2025-11-25 14:44:54 -0500

    main - add express

---
+++ 01-advanced/week16-cookies-auth/express-cookies-auth/src/index-00-no-encryption.ts
@@ -0,0 +1,65 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = 5500;
+
+app.use(cookieParser()); // Poblar req.cookies con objetos de cookies
+app.use(express.json()); // Transformar req.body a JSON
+
+// Arreglo de usuarios en memoria:
+// El id será un string ya que usaremos 'crypto.randomUUID()' para generarlo
+const users: { id: string; email: string; password: string }[] = [];
+
+app.post("/signup", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((u) => u.email === email);
+
+  if (user) {
+    res.status(400).send("El correo ya está registrado");
+    return;
+  }
+
+  const newUser = {
+    id: crypto.randomUUID(),
+    email,
+    password,
+  };
+
+  users.push(newUser);
+
+  res.status(201).json(newUser);
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((u) => u.email === email && u.password === password);
+
+  if (user) {
+    res.cookie("userId", user.id, { httpOnly: true });
+    res.send("Login exitoso");
+  } else {
+    res.status(401).send("Credenciales incorrectas");
+  }
+});
+
+app.get("/user", (req, res) => {
+  const userId = req.cookies.userId;
+
+  const user = users.find((u) => u.id === userId);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(403).send("Acceso denegado");
+  }
+});
+
+app.post("/logout", (_req, res) => {
+  res.clearCookie("userId");
+  res.send("Logout exitoso");
+});
+
+app.listen(port, () => console.log(`Escuchando al puerto ${port}`));
---
+++ 01-advanced/week16-cookies-auth/express-cookies-auth/src/index.ts
@@ -0,0 +1,75 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import bcrypt from "bcrypt";
+
+const app = express();
+const port = 5500;
+
+app.use(cookieParser()); // Poblar req.cookies con objetos de cookies
+app.use(express.json()); // Transformar req.body a JSON
+
+// Arreglo de usuarios en memoria:
+// El id será un string ya que usaremos 'crypto.randomUUID()' para generarlo
+const users: { id: string; email: string; password: string }[] = [];
+
+app.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((u) => u.email === email);
+
+  if (user) {
+    res.status(400).send("El correo ya está registrado");
+    return;
+  }
+
+  const costFactor = 10; // Cost factor
+  const hashedPassword = await bcrypt.hash(password, costFactor);
+
+  const newUser = {
+    id: crypto.randomUUID(),
+    email: req.body.email,
+    password: hashedPassword,
+  };
+
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((u) => u.email === email);
+
+  if (!user) {
+    res.status(401).send("Credenciales incorrectas");
+    return;
+  }
+
+  const isValid = await bcrypt.compare(password, user.hashedPassword);
+
+  if (isValid) {
+    res.cookie("userId", user.id, { httpOnly: true });
+    res.send("Login exitoso");
+  } else {
+    res.status(401).send("Credenciales incorrectas");
+  }
+});
+
+app.get("/user", (req, res) => {
+  const userId = req.cookies.userId;
+
+  const user = users.find((u) => u.id === userId);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(403).send("Acceso denegado");
+  }
+});
+
+app.post("/logout", (_req, res) => {
+  res.clearCookie("userId");
+  res.send("Logout exitoso");
+});
+
+app.listen(port, () => console.log(`Escuchando al puerto ${port}`));

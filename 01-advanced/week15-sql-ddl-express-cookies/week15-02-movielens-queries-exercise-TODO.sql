6df27920b4b736f64ce047300c7ef8c415a4c8a7
Ken Vega <ken.vega.g@gmail.com>
2025-12-15 14:16:45 -0500

    main - add sql joins

---
+++ 01-advanced/week15-sql-ddl-express-cookies/week15-02-movielens-queries-exercise-TODO.sql
@@ -0,0 +1,11 @@
+-- Listar todas las columnas de la tabla genres
+-- Listar todas las columnas de la tabla movies
+-- Listar el title y release_date de todas las películas estrenadas en 1995-01-01
+-- Listar el title y release_date de todas las películas estrenadas entre 1996-01-01 y 1998-01-01
+-- Listar el title de todas las películas que empiezan con la letra 'F'
+-- Listar el title de la película más antigua
+-- Listar el title y release_date de todas las películas ordenadas desde la más nueva hasta la más antigua
+-- Listar todos los años en donde se estrenó alguna película sin tener valores repetidos
+-- Listar la cantidad de usuarios agrupados por hombres y mujeres
+-- Listar la cantidad de usuarios agrupados por edad
+-- Lista la edad promedio de los usuarios agrupados por género

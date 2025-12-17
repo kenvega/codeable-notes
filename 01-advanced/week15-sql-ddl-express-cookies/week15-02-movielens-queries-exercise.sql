58de9399c65dfc2e3c97f462072b4aa13b599526
Ken Vega <ken.vega.g@gmail.com>
2025-12-17 12:26:06 -0500

    main - finish queries exercises

--- 01-advanced/week15-sql-ddl-express-cookies/week15-02-movielens-queries-exercise.sql
+++ 01-advanced/week15-sql-ddl-express-cookies/week15-02-movielens-queries-exercise.sql
@@ -49,7 +49,47 @@ LIMIT
   1;
 
 -- Listar el title y release_date de todas las películas ordenadas desde la más nueva hasta la más antigua
+SELECT
+  title,
+  release_date
+FROM
+  movies
+ORDER BY
+  release_date DESC;
+
 -- Listar todos los años en donde se estrenó alguna película sin tener valores repetidos
+SELECT
+  DISTINCT EXTRACT(
+    YEAR
+    FROM
+      release_date
+  ) AS year
+FROM
+  movies;
+
 -- Listar la cantidad de usuarios agrupados por hombres y mujeres
+SELECT
+  gender,
+  COUNT(*) AS user_count
+FROM
+  users
+GROUP BY
+  gender;
+
 -- Listar la cantidad de usuarios agrupados por edad
+SELECT
+  age,
+  COUNT(*) AS user_count
+FROM
+  users
+GROUP BY
+  age;
+
 -- Lista la edad promedio de los usuarios agrupados por género
+SELECT
+  gender,
+  AVG(age) AS average_age
+FROM
+  users
+GROUP BY
+  gender;
